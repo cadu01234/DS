98e482dfacc5f11174d9570102d4edbe56b68e38
Cadu_JVN <145131105+cadu01234@users.noreply.github.com>
2024-03-19 08:12:18 -0300

    Add files via upload

---
+++ Aula02/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+	public static void main(String [] args) {
+		// TODO Auto-generated constructor stub
+		Imc pessoa1 = new Imc();
+		
+		pessoa1.atributosImc();
+		System.out.println(pessoa1.calcularImc());
+	}
+}
