0de35f3b8683d3c2bd8e4ad9068e7e9827c9adee
Cadu_JVN <145131105+cadu01234@users.noreply.github.com>
2024-04-10 08:12:41 -0300

    Filme.java
    1) Crie uma classe TestarFilme que possua um método main de modo que seja
    
    possível testar a classe Filme criada na questão anterior.
    
    Crie um objeto filme1 do tipo Filme.
    
    Altere o atributo título para “Os Vingadores”.
    
    Altere o atributo duracaoEmMinutos para 142.
    
    Chame o método exibirDuracaoEmHoras() para mostrar quantas horas o filme possui.
    
    Crie um objeto filme2 do tipo Filme.
    
    Altere o atributo título do filme2 para “Hotel Transilvânia”.
    
    Altere o atributo duracaoEmMinutos do filme2 para 93.
    
    Chame o método exibirDuracaoEmHoras() do filme2 para mostrar quantas horas o filme possui.
    
    Exiba a mensagem: “Os filmes em cartaz são X e Y”, onde no lugar de X,Deverá aparecer o título do filme1 e no lugar de Y deverá aparecer o título do filme2.
    
    Instancie outros 3 filmes e faça as mesmas ações descritas cima, porém utilizando novos valores.

---
+++ AulaFilme/Filme.java
@@ -0,0 +1,31 @@
+public class Filme {
+    private String titulo;
+    private int duracao;
+ 
+    public Filme(String titulo, int duracao) {
+        this.titulo = titulo;
+        this.duracao = duracao;
+    }
+ 
+    public String getTitulo() {
+        return titulo;
+    }
+ 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+ 
+    public int getDuracao() {
+        return duracao;
+    }
+ 
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+ 
+    public String exibirDuracaoEmHoras() {
+        int horas = duracao / 60;
+        int minutos = duracao % 60;
+        return horas + "h " + minutos + "min";
+    }
+}
