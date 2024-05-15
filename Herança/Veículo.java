c65bddf19e632d7c012d8350f92baba674bd38d0
Cadu_JVN <145131105+cadu01234@users.noreply.github.com>
2024-05-15 07:58:52 -0300

    Veículo.java

---
+++ Herança/Veículo.java
@@ -0,0 +1,39 @@
+public class Veiculo {
+	
+	private String marca;
+	private String modelo;
+	private int ano;
+	private String cor;
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public void Imprime() {
+		System.out.println(this.marca);
+		System.out.println(this.modelo);
+		System.out.println(this.ano);
+		System.out.println(this.cor);
+	}
+}
