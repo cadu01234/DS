d1e4a67f8d648862849e44b8fc6418d86978c3fc
Cadu_JVN <145131105+cadu01234@users.noreply.github.com>
2024-05-29 07:57:11 -0300

    Revista.java

--- Prova/Revista.java
+++ Prova/Revista.java
@@ -1,13 +1,17 @@
 
-public class Revista extends Publicacao { 
-	
+public class Revista extends Publicacao{
 	private int numeroEdicao;
 	private String mesPublicacao;
+	
+	public Revista (String titulo, String autor, int anoPublicacao, int numeroEdicao, String mesPublicacao) {
+		super(titulo, autor, anoPublicacao);
+		this.numeroEdicao = numeroEdicao;
+		this.mesPublicacao = mesPublicacao;
+	}
 	
 	public int getNumeroEdicao() {
 		return numeroEdicao;
 	}
-	
 	public void setNumeroEdicao(int numeroEdicao) {
 		this.numeroEdicao = numeroEdicao;
 	}
@@ -17,11 +21,17 @@ public class Revista extends Publicacao {
 	public void setMesPublicacao(String mesPublicacao) {
 		this.mesPublicacao = mesPublicacao;
 	}
-
-@Override
-
+	
+	@Override
 	public void exibirDetalhes() {
-		System.out.println("Deseja incluir o mês de publicação? (1-s ou 2-n");
+		System.out.println(" Edição Número: " + numeroEdicao + " Mês da Publicação: " + mesPublicacao);
+	}
+	public void exibirDetalhes(boolean temMes) {
+		super.exibirDetalhes();
+		System.out.println(" Edição Número: " + numeroEdicao);
+		if (temMes) {
+			System.out.println(" Mês da Publicação: " + mesPublicacao);
+		}
+	
 }
-
 }
