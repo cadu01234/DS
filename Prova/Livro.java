66110048d729a85c45806a3ae61e0de03a5a15a9
Cadu_JVN <145131105+cadu01234@users.noreply.github.com>
2024-05-29 07:44:56 -0300

    Livro.java

--- Prova/Livro.java
+++ Prova/Livro.java
@@ -1,30 +1,38 @@
-
 public class Livro extends Publicacao {
 	private int numeroPaginas;
 	private String editora;
 	
-	public int getNumeroPaginas() {
+	
+	public Livro (String titulo, String autor, int anoPublicacao, int numeroPaginas, String editora) {
+		super(titulo, autor, anoPublicacao);
+		this.numeroPaginas = numeroPaginas;
+		this.editora = editora;
+	}
+ 
+	public int getNumeropaginas() {
 		return numeroPaginas;
 	}
-	
-	public void setNumeroPaginas(int numeroPaginas) {
-		this.numeroPaginas = numeroPaginas;
+	public void setNumeropaginas(int numeropaginas) {
+		this.numeroPaginas = numeropaginas;
 	}
-	
 	public String getEditora() {
 		return editora;
 	}
-
 	public void setEditora(String editora) {
 		this.editora = editora;
 	}
-
-@Override
-
+	
+	@Override
 	public void exibirDetalhes() {
-		System.out.println("Deseja incluir a editora? (1-s ou 2-n");
-}
+		super.exibirDetalhes();
+		System.out.println(" Numero de Paginas: " + numeroPaginas + " Editora: " + editora);
+	}
 	
-	
-
+	public void exibirDetalhes(boolean temEditora) {
+		super.exibirDetalhes();
+		System.out.println(" Numero de Paginas: " + numeroPaginas);
+		if (temEditora) {
+			System.out.println(" Editora: " + editora);
+		}
+	}
 }
