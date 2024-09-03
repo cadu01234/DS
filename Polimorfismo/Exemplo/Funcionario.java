a3a9c25f90868cd68adcc4dedc9c6ebb317438ea
Carlos Silva <145131105+cadu01234@users.noreply.github.com>
2024-09-03 08:22:27 -0300

    Funcionario.java

---
+++ Polimorfismo/Exemplo/Funcionario.java
@@ -0,0 +1,34 @@
+
+public class Funcionario {
+	
+	private String nome;
+	private String cpf;
+	protected int salario;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getSalario() {
+		return salario;
+	}
+	public void setSalario(int salario) {
+		this.salario = salario;
+	}
+	
+	//Método que retorna o valor da bonificação do funcionario
+	public double getBonificacao() {
+		
+		return salario * 0.10;
+	}
+	
+	
+}
